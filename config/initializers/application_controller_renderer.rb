5aec5b47e4e0d3d34cdcb5f1f37bff89377f974f
junpppppppei <junpei523@gmail.com>
2022-02-05 10:02:59 +0900

    Add

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '75550e7d58f0842cad0f408cc905702f1d8aa73edb9ce04dbda5a8c0896e7dd8dcfab86abc1e56f74cf9425b1176bc9c12092bdb7f930aa7968666277dc727e6'
